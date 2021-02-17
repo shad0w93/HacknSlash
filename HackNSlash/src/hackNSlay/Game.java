ea708893bcacf61ab759e0ca0c3aea76de4ed341
Stephan Stenzel <stephan.stenzel@konicaminolta.de>
2021-02-17 11:11:32 +0100

    Initial Commit

---
+++ HackNSlash/src/hackNSlay/Game.java
@@ -0,0 +1,50 @@
+package hackNSlay;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.geom.*;
+
+public class Game extends BasicGame {
+
+    private Input input;
+    private Rectangle mainGame;
+    private int xMainGame;
+    private int yMainGame;
+
+    public Game() {
+        super("Hack´n´Slash");
+    }
+
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer container = new AppGameContainer(new Game());
+        container.setDisplayMode(1500, 1000, false);
+        container.start();
+    }
+
+    @Override
+    public void render(GameContainer container, Graphics g) throws SlickException {
+        g.setColor(Color.darkGray);
+        g.fill(mainGame);
+    }
+
+    @Override
+    public void init(GameContainer container) throws SlickException {
+        input = container.getInput();
+        setGameField();
+        container.setMinimumLogicUpdateInterval(40);
+        container.setMaximumLogicUpdateInterval(40);
+
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+        if (input.isKeyPressed(Input.KEY_ESCAPE)) {
+            container.exit();
+        }
+    }
+
+    public void setGameField() {
+        xMainGame = 0;
+        yMainGame = 0;
+        mainGame = new Rectangle(xMainGame,yMainGame,1000,1000);
+    }
+}
