e2ec4cc9b54890a1d72022f1a3429d3ee2678fb0
joshu <joshu@LAPTOP-VK1MFD32.fritz.box>
2021-03-06 23:10:21 +0100

    Türen müssen nurnoch auf die gleiche höhe gemacht werden. Ansonsten werden jetzt 3x3 Räume generiert mit türen, sodass man alle Räume erreichen kann.

--- HackNSlash/src/levelgenerator/Dungeon.java
+++ HackNSlash/src/levelgenerator/Dungeon.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
@@ -15,28 +16,64 @@ public class Dungeon {
 	public int level;
 	public int dungeonWidth;
 	public int dungeonHeight;
-	
-	public int levelPosition[][] = new int[3][3];
-	public ArrayList<Rectangle> walls;
-	public ArrayList<Rectangle> doors;
-	//[Reihe][spalte][0 = Oben, 1 = Rechts, 2 = Unten, 3 = Links];
-	public int levelRoomDoors[][][] = new int[3][3][4];
-	
+
+	static int maxXPosition = 3;
+	static int maxYPosition = 3;
+	public int levelPositionX = 1;
+	public int levelPositionY = 1;
+	@SuppressWarnings("unchecked")
+	public ArrayList<Rectangle> walls[][];
+	public ArrayList<Rectangle> doors[][];
+
 	public Dungeon(int width, int height) {
+		walls = new ArrayList[maxXPosition][maxYPosition];
+		doors = new ArrayList[maxXPosition][maxYPosition];
 		dungeonWidth = width;
 		dungeonHeight = height;
-		walls = new ArrayList<Rectangle>();
-		doors = new ArrayList<Rectangle>();
+		for (int i = 0; i < walls.length; i++) {
+			for (int y = 0; y < walls[0].length; y++) {
+				walls[i][y] = new ArrayList<Rectangle>();
+			}
+		}
+		for (int i = 0; i < walls.length; i++) {
+			for (int y = 0; y < walls[0].length; y++) {
+				doors[i][y] = new ArrayList<Rectangle>();
+			}
+		}
 		dungeonState = new WoodenForest(this);
 	}
-	
-	public void render(GameContainer container, Graphics g) throws SlickException{
+
+	public void render(GameContainer container, Graphics g) throws SlickException {
 		g.setColor(Color.green);
-		for (Rectangle rectangle : walls) {
+		for (Rectangle rectangle : walls[levelPositionX][levelPositionY]) {
 			g.fill(rectangle);
 		}
+		/*
+		 * g.setColor(Color.blue); for (Rectangle rectangle : doors) {
+		 * g.fill(rectangle); }
+		 */
 	};
-	
-	public void update(GameContainer container, int delta) throws SlickException{
+
+	public void update(GameContainer container, int delta, Input input) throws SlickException {
+		if (input.isKeyPressed(Input.KEY_UP)) {
+			if(levelPositionY < maxYPosition -1) {
+				levelPositionY++;
+			}
+		}
+		if (input.isKeyPressed(Input.KEY_DOWN)) {
+			if(levelPositionY > 0) {
+				levelPositionY--;
+			}
+		}
+		if (input.isKeyPressed(Input.KEY_RIGHT)) {
+			if(levelPositionX < maxYPosition -1) {
+				levelPositionX++;
+			}
+		}
+		if (input.isKeyPressed(Input.KEY_LEFT)) {
+			if(levelPositionX > 0) {
+				levelPositionX--;
+			}
+		}
 	};
 }
--- HackNSlash/src/levelgenerator/WoodenForest.java
+++ HackNSlash/src/levelgenerator/WoodenForest.java
@@ -5,17 +5,37 @@ import java.util.Random;
 
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.geom.Shape;
+import org.w3c.dom.css.Rect;
 
 public class WoodenForest implements DungeonState {
 	public int level;
 	public static int rectangleSize = 25;
 	Random random;
+	// [Reihe][spalte][0 = Oben, 1 = Rechts, 2 = Unten, 3 = Links];
+	public int[][][] levelRoomDoors;
 
 	public WoodenForest(Dungeon dungeon) {
+		levelRoomDoors = new int[dungeon.walls.length][dungeon.walls[0].length][4];
 		random = new Random();
 		drawField(dungeon);
+		for (int xWalls = 0; xWalls < dungeon.walls.length; xWalls++) {
+			for (int yWalls = 0; yWalls < dungeon.walls[xWalls].length; yWalls++) {
+				generateWalls(dungeon, xWalls, yWalls);
+			}
+		}
+		for (int xWalls = 0; xWalls < dungeon.walls.length; xWalls++) {
+			for (int yWalls = 0; yWalls < dungeon.walls[xWalls].length; yWalls++) {
+				generateWalls(dungeon, xWalls, yWalls);
+			}
+		}
+		// Hier wird nicht einfach nur durch das Array gegangen das hier immer nach
+		// einer anderen Logik die Türen generiert werden
 		generateDoors(dungeon);
-		generateWalls(dungeon);
+		for (int xWalls = 0; xWalls < dungeon.walls.length; xWalls++) {
+			for (int yWalls = 0; yWalls < dungeon.walls[xWalls].length; yWalls++) {
+				setRoomDoors(dungeon, xWalls, yWalls);
+			}
+		}
 		generateMobs(dungeon);
 	}
 
@@ -24,93 +44,226 @@ public class WoodenForest implements DungeonState {
 
 	}
 
+	public void setRoomDoors(Dungeon dungeon, int i, int f) {
+		for (int d = 0; d < levelRoomDoors[0][0].length; d++) {
+			if (levelRoomDoors[i][f][d] == 1) {
+				if (d == 0) {// Oben
+					int doorY = 0;
+					int xLength = dungeon.dungeonWidth - (rectangleSize * 3);
+					int doorX = 0 + (int) (Math.random() * ((xLength - 0) + 1));
+					while (doorX % 25 != 0) {
+						doorX++;
+					}
+					dungeon.doors[i][f].add(new Rectangle(doorX, doorY, rectangleSize, rectangleSize));
+				} else if (d == 1) { // Rechts
+					int doorX = dungeon.dungeonWidth - rectangleSize;
+					int yLength = dungeon.dungeonHeight - (rectangleSize * 3);
+					int doorY = 0 + (int) (Math.random() * ((yLength - 0) + 1));
+					while (doorY % 25 != 0) {
+						doorY++;
+					}
+					dungeon.doors[i][f].add(new Rectangle(doorX, doorY, rectangleSize, rectangleSize));
+				} else if (d == 2) { // Unten
+					int doorY = dungeon.dungeonHeight - rectangleSize;
+					int xLength = dungeon.dungeonWidth - (rectangleSize * 3);
+					int doorX = 0 + (int) (Math.random() * ((xLength - 0) + 1));
+					while (doorX % 25 != 0) {
+						doorX++;
+					}
+					dungeon.doors[i][f].add(new Rectangle(doorX, doorY, rectangleSize, rectangleSize));
+				} else if (d == 3) { // links
+					int doorX = 0;
+					int yLength = dungeon.dungeonHeight - (rectangleSize * 3);
+					int doorY = 0 + (int) (Math.random() * ((yLength - 0) + 1));
+					while (doorY % 25 != 0) {
+						doorY++;
+					}
+					dungeon.doors[i][f].add(new Rectangle(doorX, doorY, rectangleSize, rectangleSize));
+				}
+			}
+		}
+		destroyWall(dungeon, i, f);
+	}
+
+	public void destroyWall(Dungeon dungeon, int roomX, int roomY) {
+		for (Rectangle rectangle : dungeon.doors[roomX][roomY]) {
+			for (int i = dungeon.walls[roomX][roomY].size() - 1; i >= 0; i--) {
+				Rectangle c = dungeon.walls[roomX][roomY].get(i);
+				if (c.getCenterX() == rectangle.getCenterX() && c.getCenterY() == rectangle.getCenterY()) {
+					dungeon.walls[roomX][roomY].remove(i);
+				}
+			}
+		}
+	}
+
 	@Override
 	public void generateDoors(Dungeon dungeon) {
-		int middleX = dungeon.levelRoomDoors.length / 2;
-		int middleY = dungeon.levelRoomDoors[0].length / 2;
-
-		//System.out.println(middleX);
-		//System.out.println(middleY);
+		int middleX = levelRoomDoors.length / 2;
+		int middleY = levelRoomDoors[0].length / 2;
 
 		// Start in the middle
 		boolean middleDoorsGenerated = false;
 		while (middleDoorsGenerated == false) {
-			for (int i = 0; i < dungeon.levelRoomDoors[middleX][middleY].length; i++) {
-				dungeon.levelRoomDoors[middleX][middleY][i] = random.nextInt(2);
-				System.out.println(dungeon.levelRoomDoors[middleX][middleY][i]);
+			for (int i = 0; i < levelRoomDoors[middleX][middleY].length; i++) {
+				levelRoomDoors[middleX][middleY][i] = random.nextInt(2);
 			}
-			for (int i = 0; i < dungeon.levelRoomDoors[middleX][middleY].length; i++) {
-				if (dungeon.levelRoomDoors[middleX][middleY][i] == 1) {
-					middleDoorsGenerated = true;
+			int counter = 0;
+			for (int i = 0; i < levelRoomDoors[middleX][middleY].length; i++) {
+				if (levelRoomDoors[middleX][middleY][i] == 1) {
+					counter++;
 				}
 			}
+			if (counter > 1) {
+				middleDoorsGenerated = true;
+			}
 		}
+		// Jetzt in der Mitte nach oben und unten gehen
+		int tempSaveMiddleY = middleY;
+		middleY += 1;
+		while (middleY < levelRoomDoors[0].length) {
+			generateRoomDoors(dungeon, middleX, middleY);
+			middleY += 1;
+		}
+
+		middleY = tempSaveMiddleY;
+		middleY -= 1;
+		while (middleY != -1) {
+			generateRoomDoors(dungeon, middleX, middleY);
+			middleY -= 1;
+		}
+
+		// Jetzt nach Rechts und dort immer nach oben und unten
+		middleY = tempSaveMiddleY;
 		int tempSaveMiddleX = middleX;
-		while (middleX < dungeon.levelRoomDoors.length) {
+		middleX += 1;
+		while (middleX < levelRoomDoors.length) {
+			tempSaveMiddleY = middleY;
+			while (middleY < levelRoomDoors[0].length) {
+				generateRoomDoors(dungeon, middleX, middleY);
+				middleY += 1;
+			}
+
+			middleY = tempSaveMiddleY;
+			while (middleY != -1) {
+				generateRoomDoors(dungeon, middleX, middleY);
+				middleY -= 1;
+			}
 			middleX += 1;
-			generateRoomDoors(dungeon, middleX, middleY);
 		}
+
+		// Zuletzt das selbe noch nach Links
 		middleX = tempSaveMiddleX;
-		while(middleX >= 0) {
+		middleY = tempSaveMiddleY;
+		middleX -= 1;
+		while (middleX != -1) {
+			tempSaveMiddleY = middleY;
+			while (middleY < levelRoomDoors.length) {
+				generateRoomDoors(dungeon, middleX, middleY);
+				middleY += 1;
+			}
+
+			middleY = tempSaveMiddleY;
+			while (middleY != -1) {
+				generateRoomDoors(dungeon, middleX, middleY);
+				middleY -= 1;
+			}
 			middleX -= 1;
-			generateRoomDoors(dungeon, middleX, middleY);
 		}
-		
-		
+
 	}
-	
-	public void generateRoomDoors(Dungeon dungeon, int roomX,int roomY) {
-		int[] roomsAroundX = {roomX +1, roomX, roomX -1, roomX};
-		int[] roomsAroundY = {roomY, roomY +1, roomY, roomY -1};
-		boolean[][][] doorCanBeGenerated = new boolean[dungeon.levelRoomDoors.length][dungeon.levelRoomDoors[0].length][dungeon.levelRoomDoors[0][0].length];
-		
-		//Prüfe ob außenrum nochmal ein Raum existiert, da man ja ansonsten dort auch keine Tür hinmachen kann.
-		for(int i = 0; i < dungeon.levelRoomDoors.length; i++) {
-			//Raum existiert?
-			if(roomsAroundX[i] <= dungeon.levelRoomDoors.length && roomsAroundY[i] <= dungeon.levelRoomDoors[i].length) {
-				//Ist von der aktuellen Türgenerationsposition (oben unten etc...) das gegenteil
-				int otherSide;
-				if(i == 0) {
-					otherSide = 2;
-				} else if (i == 1) {
-					otherSide = 3;
-				} else if (i == 2) {
-					otherSide = 0;
-				} else if (i == 3) {
-					otherSide = 1;
+
+	public void generateRoomDoors(Dungeon dungeon, int roomX, int roomY) {
+		int[] roomsAroundX = { roomX, roomX + 1, roomX, roomX - 1 };
+		int[] roomsAroundY = { roomY + 1, roomY, roomY - 1, roomY };
+		boolean[] doorCanBeThere = new boolean[4];
+		boolean[] otherRoomHasAlreadyADoorThere = new boolean[4];
+
+		// doorSide: 0 = Oben, 1 = Rechts, 2 = Unten, 3 = Links
+		// Erstmal ist jede Seite eine Seite mit einer möglichkeit für eine Tür. Erst
+		// wenn auf der anderen Seite schon eine ist kann hier nicht noch eine erstellt
+		// werden
+		for (int i = 0; i < doorCanBeThere.length; i++) {
+			doorCanBeThere[i] = true;
+		}
+		// Erstmal hat außenrum kein Raum eine Tür
+		for (int i = 0; i < otherRoomHasAlreadyADoorThere.length; i++) {
+			otherRoomHasAlreadyADoorThere[i] = false;
+		}
+		boolean enoughDoorsAreThere = false;
+		while (!enoughDoorsAreThere) {
+			for (int doorSide = 0; doorSide < levelRoomDoors[0][0].length; doorSide++) {
+				// von dem Anderen Raum die Tür Position berechnen
+				int otherDoorSide = 0;
+				if (doorSide == 0) {
+					otherDoorSide = 2;
+				} else if (doorSide == 1) {
+					otherDoorSide = 3;
+				} else if (doorSide == 2) {
+					otherDoorSide = 0;
+				} else if (doorSide == 3) {
+					otherDoorSide = 1;
 				}
-				//Ist denn in dem Raum der daran anschließt bereits eine Tür (Sollte dort ein Raum existieren)?
-				if(dungeon.levelRoomDoors[roomsAroundX[i]][roomsAroundY[i]][otherSide] == 1) {
-					doorCanBeGenerated[roomX][roomY][i] = true;
+				try {
+					if (levelRoomDoors[roomsAroundX[doorSide]][roomsAroundY[doorSide]][otherDoorSide] == 1) {
+						otherRoomHasAlreadyADoorThere[doorSide] = true;
+						levelRoomDoors[roomX][roomY][doorSide] = 1;
+					} else {
+						levelRoomDoors[roomX][roomY][doorSide] = random.nextInt(2);
+						levelRoomDoors[roomsAroundX[doorSide]][roomsAroundY[doorSide]][otherDoorSide] = levelRoomDoors[roomX][roomY][doorSide];
+					}
+				} catch (Exception e) {
+					doorCanBeThere[doorSide] = false;
+					// Es gibt keine raum im bereich -1
+				}
+			}
+			int enoughtDoorsCounter = 0;
+			for (int counter = 0; counter < levelRoomDoors[0][0].length; counter++) {
+				if (doorCanBeThere[counter] && !otherRoomHasAlreadyADoorThere[counter]
+						&& levelRoomDoors[roomX][roomY][counter] == 1) {
+					enoughDoorsAreThere = true;
+					if(roomX == 0 && roomY == 1) {
+						System.out.println(levelRoomDoors[roomX][roomY][counter]);
+					}
+					if(roomX == 2 && roomY == 1) {
+						System.out.println(levelRoomDoors[roomX][roomY][counter]);
+					}
 				} else {
-					int checkRoomX;
-					int checkRoomY;
-					boolean nextRoomHasNoDoors = false;
-					for (int y = 0; y < dungeon.levelRoomDoors[middleX][middleY].length; y++) {
-						if (dungeon.levelRoomDoors[middleX][middleY][i] == 1) {
-							middleDoorsGenerated = true;
+					if (!doorCanBeThere[counter] || otherRoomHasAlreadyADoorThere[counter]) {
+						enoughtDoorsCounter++;
+						if(roomX == 0 && roomY == 1) {
+							System.out.println(enoughtDoorsCounter);
+						}
+						if(roomX == 2 && roomY == 1) {
+							System.out.println(enoughtDoorsCounter);
 						}
 					}
 				}
 			}
+			if (enoughtDoorsCounter == levelRoomDoors[0][0].length) {
+				enoughDoorsAreThere = true;
+				if(roomX == 0 && roomY == 1) {
+					System.out.println(enoughtDoorsCounter);
+				}
+				if(roomX == 2 && roomY == 1) {
+					System.out.println(enoughtDoorsCounter);
+				}
+			}
 		}
-		
-		
 	}
-	
+
 	@Override
-	public void generateWalls(Dungeon dungeon) {
+	public void generateWalls(Dungeon dungeon, int roomX, int roomY) {
 		int x;
 		int y;
-		boolean[][][] doorIsGenerated = new boolean[dungeon.levelRoomDoors.length][dungeon.levelRoomDoors[0].length][dungeon.levelRoomDoors[0][0].length];
+		boolean[][][] doorIsGenerated = new boolean[levelRoomDoors.length][levelRoomDoors[0].length][levelRoomDoors[0][0].length];
 		// TopWalls
 		x = 0;
 		y = 0;
 		while (x < dungeon.dungeonWidth) {
-			if (x == 0 || x == (dungeon.dungeonWidth - rectangleSize)) {
-				dungeon.walls.add(new Rectangle(x, y, rectangleSize, rectangleSize));
+			if (x == 0 || x == (dungeon.dungeonWidth - (rectangleSize *3))) {
+				dungeon.walls[roomX][roomY].add(new Rectangle(x, y, rectangleSize, rectangleSize));
 			} else {
-				dungeon.walls.add(new Rectangle(x, y, rectangleSize, rectangleSize));
+				dungeon.walls[roomX][roomY].add(new Rectangle(x, y, rectangleSize, rectangleSize));
 			}
 			x += rectangleSize;
 		}
@@ -119,9 +272,9 @@ public class WoodenForest implements DungeonState {
 		y = rectangleSize;
 		while (y < (dungeon.dungeonHeight - rectangleSize)) {
 			if (y == 0 || y == (dungeon.dungeonWidth - rectangleSize)) {
-				dungeon.walls.add(new Rectangle(x, y, rectangleSize, rectangleSize));
+				dungeon.walls[roomX][roomY].add(new Rectangle(x, y, rectangleSize, rectangleSize));
 			} else {
-				dungeon.walls.add(new Rectangle(x, y, rectangleSize, rectangleSize));
+				dungeon.walls[roomX][roomY].add(new Rectangle(x, y, rectangleSize, rectangleSize));
 			}
 			y += rectangleSize;
 		}
@@ -131,24 +284,21 @@ public class WoodenForest implements DungeonState {
 		y = dungeon.dungeonHeight - rectangleSize;
 		while (x < dungeon.dungeonWidth) {
 			if (x == 0 || x == (dungeon.dungeonWidth - rectangleSize)) {
-				dungeon.walls.add(new Rectangle(x, y, rectangleSize, rectangleSize));
+				dungeon.walls[roomX][roomY].add(new Rectangle(x, y, rectangleSize, rectangleSize));
 			} else {
-				dungeon.walls.add(new Rectangle(x, y, rectangleSize, rectangleSize));
+				dungeon.walls[roomX][roomY].add(new Rectangle(x, y, rectangleSize, rectangleSize));
 			}
 			x += rectangleSize;
 		}
 		// RightWalls
 		x = dungeon.dungeonWidth - rectangleSize;
 		y = rectangleSize;
-		System.out.println(x);
-
 		while (y < (dungeon.dungeonHeight - rectangleSize)) {
 			if (y == 0 || y == (dungeon.dungeonWidth - rectangleSize)) {
-				dungeon.walls.add(new Rectangle(x, y, rectangleSize, rectangleSize));
+				dungeon.walls[roomX][roomY].add(new Rectangle(x, y, rectangleSize, rectangleSize));
 			} else {
-				dungeon.walls.add(new Rectangle(x, y, rectangleSize, rectangleSize));
+				dungeon.walls[roomX][roomY].add(new Rectangle(x, y, rectangleSize, rectangleSize));
 			}
-			System.out.println(y);
 			y += rectangleSize;
 		}
 	}
