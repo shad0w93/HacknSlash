a93bd50fb5c0775d48773e935e326d12bb7b4dd7
shad0w93 <65009081+shad0w93@users.noreply.github.com>
2021-03-10 12:43:52 +0100

    Dev stephan (#1)
    * Ändern des Update Interval (Game.java)
    
    * Erster Entwurf der Enemies
    
    * new classes
    
    * added normal Gnome Movement
    
    * Verbesserung der NormalGnome Bewebung
    
    * Medium/Hard Gnome added
    Easy Gnome WIP
    
    * EasyGnome adjusted
    
    * Erster fertiger entwurf der Gnom-states
    
    Co-authored-by: joshu <joshu@LAPTOP-VK1MFD32.fritz.box>

---
+++ HackNSlash/src/enemies/Enemy.java
@@ -0,0 +1,8 @@
+package enemies;
+
+import org.newdawn.slick.geom.Circle;
+
+public abstract class Enemy extends Being {
+	Circle circle;
+	float circleRadius;
+}
--- HackNSlash/src/hackNSlay/Game.java
+++ HackNSlash/src/hackNSlay/Game.java
@@ -3,48 +3,55 @@ package hackNSlay;
 import org.newdawn.slick.*;
 import org.newdawn.slick.geom.*;
 
+import enemies.*;
+
 public class Game extends BasicGame {
 
-    private Input input;
-    private Rectangle mainGame;
-    private int xMainGame;
-    private int yMainGame;
+	private Input input;
+	private Rectangle mainGame;
+	private int xMainGame;
+	private int yMainGame;
+	Gnome gnom;
 
-    public Game() {
-        super("Hack´n´Slash");
-    }
+	public Game() {
+		super("Hack�n�Slash");
+		gnom = new Gnome();
+	}
 
-    public static void main(String[] args) throws SlickException {
-        AppGameContainer container = new AppGameContainer(new Game());
-        container.setDisplayMode(1500, 1000, false);
-        container.start();
-    }
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer container = new AppGameContainer(new Game());
+		container.setDisplayMode(1500, 1000, false);
+		container.start();
+	}
 
-    @Override
-    public void render(GameContainer container, Graphics g) throws SlickException {
-        g.setColor(Color.darkGray);
-        g.fill(mainGame);
-    }
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		g.setColor(Color.darkGray);
+		g.fill(mainGame);
+		gnom.render(container, g);
+	}
 
-    @Override
-    public void init(GameContainer container) throws SlickException {
-        input = container.getInput();
-        setGameField();
-        container.setMinimumLogicUpdateInterval(5);
-        container.setMaximumLogicUpdateInterval(5);
+	@Override
+	public void init(GameContainer container) throws SlickException {
+		input = container.getInput();
+		setGameField();
+		container.setMinimumLogicUpdateInterval(5);
+		container.setMaximumLogicUpdateInterval(5);
+	}
 
-    }
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+		if (input.isKeyPressed(Input.KEY_ESCAPE)) {
+			container.exit();
+		}
+		gnom.update(container, delta);
+	}
 
-    @Override
-    public void update(GameContainer container, int delta) throws SlickException {
-        if (input.isKeyPressed(Input.KEY_ESCAPE)) {
-            container.exit();
-        }
-    }
+	public void setGameField() {
+		xMainGame = 0;
+		yMainGame = 0;
+		mainGame = new Rectangle(xMainGame,yMainGame,1000,1000);
+		gnom = new Gnome();
 
-    public void setGameField() {
-        xMainGame = 0;
-        yMainGame = 0;
-        mainGame = new Rectangle(xMainGame,yMainGame,1000,1000);
-    }
+	}
 }
